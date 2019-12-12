756d18fb6bd55dd7e80ad55780f04bd3624b0815
adi <dev1@gmail.com>
2019-12-12 16:32:19 +0530

    hii adiking

---
+++ MyApp/src/com/tcs/myapp/MyApp.java
@@ -0,0 +1,10 @@
+package com.tcs.myapp;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("oooooooooooooooooooooooooo");
+	}
+
+}
